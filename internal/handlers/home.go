5599f2fba0f7bd1bf3c2cce22400d49643a38fa3
Amirhossein <a.pooladvand@ipaam.ir>
2024-02-12 16:24:37 +0330

    WIP

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 	"po/internal/app"
 )
 
 func Home(c echo.Context) error {
 	ctx := c.(*app.Context)
+
+	zap.L().Error("Hello World")
 
 	return ctx.R().Ok("Hello World")
 }
--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"os"
 	"po/cmd"
 )
 
 func main() {
-	//_ = os.Setenv("ELASTIC_APM_SERVER_URL", "https://127.0.0.1:8200")
-	//_ = os.Setenv("ELASTIC_APM_SERVICE_NAME", "app")
-	//_ = os.Setenv("ELASTIC_APM_SECRET_TOKEN", "")
+	_ = os.Setenv("ELASTIC_APM_SERVER_URL", "http://127.0.0.1:8200")
+	_ = os.Setenv("ELASTIC_APM_SERVICE_NAME", "app")
+	_ = os.Setenv("ELASTIC_APM_SECRET_TOKEN", "")
 	//_ = os.Setenv("ELASTIC_APM_SERVER_CA_CERT_FILE", "docker/tls/certs/ca/ca.crt")
-	//_ = os.Setenv("ELASTIC_APM_LOG_LEVEL", "debug")
-	//_ = os.Setenv("ELASTIC_APM_LOG_FILE", "stderr")
-	//_ = os.Setenv("ELASTIC_APM_ENVIRONMENT", "staging")
-	//_ = os.Setenv("ELASTIC_APM_VERIFY_SERVER_CERT", "false")
+	_ = os.Setenv("ELASTIC_APM_LOG_LEVEL", "debug")
+	_ = os.Setenv("ELASTIC_APM_LOG_FILE", "stderr")
+	_ = os.Setenv("ELASTIC_APM_ENVIRONMENT", "staging")
+	_ = os.Setenv("ELASTIC_APM_VERIFY_SERVER_CERT", "false")
 
 	cmd.Execute()
 }
