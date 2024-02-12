d1fa0627a340f3d0b31cf3ae845804f4573bc3fc
Amirhossein <a.pooladvand@ipaam.ir>
2024-02-12 17:24:27 +0330

    Implementing apm

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -2,14 +2,20 @@ package handlers
 
 import (
 	"github.com/labstack/echo/v4"
-	"go.uber.org/zap"
+	"go.elastic.co/apm/v2"
 	"po/internal/app"
 )
 
 func Home(c echo.Context) error {
 	ctx := c.(*app.Context)
 
-	zap.L().Error("Hello World")
+	tx := apm.DefaultTracer().StartTransaction("hello", "request")
+	defer tx.End()
+
+	tx.Context.SetCustom("key", "value")
+
+	span := tx.StartSpan("work", "custom", nil)
+	defer span.End()
 
 	return ctx.R().Ok("Hello World")
 }
