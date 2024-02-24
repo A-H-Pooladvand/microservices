7af8eebbbe2167017a3f839cba7ba16e5cade9f8
Amirhossein <a.pooladvand@ipaam.ir>
2024-02-24 16:12:19 +0330

    Cleanup

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -3,23 +3,10 @@ package handlers
 import (
 	"github.com/labstack/echo/v4"
 	"po/internal/app"
-	"po/internal/vault"
 )
 
 func Home(c echo.Context) error {
 	ctx := c.(*app.Context)
 
-	client, err := vault.New()
-
-	if err != nil {
-		return ctx.R().BadRequest(err.Error())
-	}
-
-	secret, err := client.Get(c.Request().Context(), "database")
-
-	if err != nil {
-		return ctx.R().BadRequest(err.Error())
-	}
-
-	return ctx.R().Ok(secret.Data)
+	return ctx.R().Ok("Ok")
 }
